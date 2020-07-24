db2cecced98230cdfe8818e76fe0205c11b7004b
codescratchblocks <34673672+codescratchblocks@users.noreply.github.com>
2020-07-24 13:25:54 +1000

    Add a "then"

--- cc-gp.lua
+++ cc-gp.lua
@@ -3,7 +3,7 @@ if tArgs[1] == "install" then
 shell.run("wget https://raw.githubusercontent.com/codescratchblocks/cc-repo/master/"..tArgs[2]..".lua /temp.lua")
 shell.run("/temp.lua")
 fs.delete("/temp.lua")
-elseif tArgs[1] == "update"
+elseif tArgs[1] == "update" then
 fs.delete("cc-gp.lua")
 shell.run("wget https://raw.githubusercontent.com/codescratchblocks/cc-repo/master/cc-gp.lua")
 print("Updated.")
